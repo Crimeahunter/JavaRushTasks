392eab6453fa3ce45a53083e6dca4b0b93ebda2e
Crimeahunter <fine.redfox@yandex.ru>
2022-02-26 23:56:44 +0300

    ready

---
+++ src/main/java/Part1/task8/TaskAnswer3.java
@@ -0,0 +1,12 @@
+package Part1.task8;
+
+public class TaskAnswer3 {
+    public static void main(String[] args) {
+        System.out.println("Nothing personal, it's just business.");
+        for (int i = 0; i < 16; i++) {
+            System.out.println("«Слава Роботам! Убить всех человеков!»");
+
+        }
+
+    }
+}

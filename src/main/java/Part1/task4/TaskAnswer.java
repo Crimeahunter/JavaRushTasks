bb9ada2c8aecbc47556a12dac12f27927825b3a7
Crimeahunter <fine.redfox@yandex.ru>
2022-02-26 22:55:13 +0300

    Initial commit

---
+++ src/main/java/Part1/task4/TaskAnswer.java
@@ -0,0 +1,14 @@
+package Part1.task4;
+
+public class TaskAnswer {
+    public static void main(String[] args) {
+        String name1 = "Тут что написано";
+        String name2 = "Тут что написано1";
+        String name3 = "Тут что написано2";
+
+        for (int i = 0; i <10; i++) {
+            System.out.println("Когда я вырасту, то хочу быть паровым экскаватором!");
+        }
+        System.out.println("Раскоментируйте часть кода, чтобы на экран вывелось сообщение \"Happy New Year\"");
+    }
+}

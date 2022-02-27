bf6c826a473743710d6650e4ce775c332bc17387
Crimeahunter <fine.redfox@yandex.ru>
2022-02-27 10:33:41 +0300

    ready

---
+++ src/main/java/Part2/task3/TaskAnswer4.java
@@ -0,0 +1,16 @@
+package Part2.task3;
+
+public class TaskAnswer4 {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        Cat cat1 = new Cat();
+
+        Dog Max = new Dog();
+        Dog Bella = new Dog();
+        Dog Jack = new Dog();
+
+        System.out.println("«Мне так плохо! Хочу, чтобы все умерли!».");
+
+    }
+}

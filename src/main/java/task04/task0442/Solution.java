8ad3ecc7dc810c05a9d8e427a21fa664ef796789
CrimeaHunter <fine.redfox@yandex.ru>
2022-03-08 20:50:40 +0300

    ready

---
+++ src/main/java/task04/task0442/Solution.java
@@ -0,0 +1,24 @@
+package task04.task0442;
+
+
+/* 
+Суммирование
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = 0;
+        int sum = 0;
+        while (a!=-1) {
+            a = Integer.parseInt(br.readLine());
+            sum += a;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/task04/task0443/Solution.java
@@ -0,0 +1,23 @@
+package task04.task0443;
+
+
+/* 
+Васян
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println("Меня зовут "+name+".");
+        System.out.print("Я родился "+d+"."+m+"."+y);
+    }
+}

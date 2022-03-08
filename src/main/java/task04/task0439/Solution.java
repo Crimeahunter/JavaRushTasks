946aa956433c7005354f66558635c303c5bbb773
CrimeaHunter <fine.redfox@yandex.ru>
2022-03-08 20:47:57 +0300

    ready

---
+++ src/main/java/task04/task0439/Solution.java
@@ -0,0 +1,21 @@
+package task04.task0439;
+
+/* 
+Мамбу
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for (int i=0;i<10;i++)
+            System.out.println(s+" любит меня.");
+
+
+    }
+}
